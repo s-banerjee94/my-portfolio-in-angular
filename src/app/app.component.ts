e827f2dfa5a7bb7f87b1b7fc383a498acf88486d
s-banerjee94 <shondha.tara94@gmail.com>
2025-03-18 22:33:23 +0530

    init setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { ButtonModule } from 'primeng/button';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'my-portfolio-angular';
