b14314a5297953a7343a7b4a22fd5928a76757f6
s-banerjee94 <shondha.tara94@gmail.com>
2025-06-30 22:28:34 +0530

    converted to template-driven form

--- src/app/dashboard/login/login.component.ts
+++ src/app/dashboard/login/login.component.ts
@@ -1,15 +1,17 @@
-import { Component, inject } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import {Component, inject} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule, NgForm} from '@angular/forms';
 
-import { CardModule } from 'primeng/card';
-import { FloatLabel } from 'primeng/floatlabel';
-import { InputTextModule } from 'primeng/inputtext';
-import { PasswordModule } from 'primeng/password';
-import { ButtonModule } from 'primeng/button';
-import { Auth } from '@angular/fire/auth';
-import { AuthService } from '../../services/auth-service.service';
-import { Router } from '@angular/router';
+import {CardModule} from 'primeng/card';
+import {FloatLabel} from 'primeng/floatlabel';
+import {InputTextModule} from 'primeng/inputtext';
+import {PasswordModule} from 'primeng/password';
+import {ButtonModule} from 'primeng/button';
+import {AuthService} from '../../services/auth-service.service';
+import {Router} from '@angular/router';
+import {MessageService} from 'primeng/api';
+import {Toast} from 'primeng/toast';
+import {Message} from 'primeng/message';
 
 @Component({
   selector: 'app-login',
@@ -21,9 +23,12 @@ import { Router } from '@angular/router';
     InputTextModule,
     PasswordModule,
     ButtonModule,
+    Toast,
+    Message,
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
+  providers: [MessageService]
 })
 export class LoginComponent {
   email: string = '';
@@ -31,16 +36,39 @@ export class LoginComponent {
 
   private authService: AuthService = inject(AuthService);
   private router: Router = inject(Router);
+  private messageService: MessageService = inject(MessageService);
 
-  login() {
-    this.authService.login(this.email, this.password).subscribe({
-      next: () => {
-        console.log('Login successful');
-        this.router.navigate(['/dashboard']);
-      },
-      error: (error) => {
-        console.error('Login failed', error);
-      },
-    });
+  login(form: NgForm) {
+    if (form.valid) {
+      this.authService.login(this.email, this.password).subscribe({
+        next: () => {
+          console.log('Login successful');
+          this.router.navigate(['/dashboard']);
+          form.resetForm();
+        },
+        error: (error) => {
+          if(error.code === "auth/user-not-found"){
+            this.messageService.add({
+              severity: 'error',
+              summary: 'Login Failed',
+              detail:  'Invalid email address. Please try again.'
+            });
+            return;
+          } else if(error.code === "auth/wrong-password"){
+            this.messageService.add({
+              severity: 'error',
+              summary: 'Login Failed',
+              detail:  'Invalid password. Please try again.'
+            });
+            return;
+          }
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Login Failed',
+            detail: 'Failed to login. Please try again.'
+          });
+        },
+      });
+    }
   }
 }
