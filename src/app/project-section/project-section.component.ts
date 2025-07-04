761a18f8d1cc5d462a1420346b6458221a546e35
s-banerjee94 <shondha.tara94@gmail.com>
2025-07-05 03:37:14 +0530

    Revamp Project section: implement PrimeNG Card and Dialog components, enhance layout and responsiveness, add "See More Projects" functionality with modal dialog. Adjust Hero section for improved display.

--- src/app/project-section/project-section.component.ts
+++ src/app/project-section/project-section.component.ts
@@ -5,10 +5,13 @@ import { ExternalLink, LucideAngularModule, Github } from 'lucide-angular';
 
 import { ChipModule } from 'primeng/chip';
 import { ProfileService } from '../services/profile-service.service';
+import {Card} from 'primeng/card';
+import {Button} from 'primeng/button';
+import {Dialog} from 'primeng/dialog';
 
 @Component({
   selector: 'app-project-section',
-  imports: [CommonModule, ChipModule, LucideAngularModule],
+  imports: [CommonModule, ChipModule, LucideAngularModule, Card, Button, Dialog],
   templateUrl: './project-section.component.html',
   styleUrl: './project-section.component.css',
 })
@@ -16,6 +19,7 @@ export class ProjectSectionComponent implements OnInit {
   readonly ExternalLink = ExternalLink;
   readonly Github = Github;
   projects: any[] = [];
+  visible: boolean = false;
 
   private profileService: ProfileService = inject(ProfileService);
 
@@ -23,5 +27,9 @@ export class ProjectSectionComponent implements OnInit {
     this.profileService.getAllProjects().subscribe((projects) => {
       this.projects = projects;
     });
+  }
+
+  showDialog() {
+    this.visible = true;
   }
 }
